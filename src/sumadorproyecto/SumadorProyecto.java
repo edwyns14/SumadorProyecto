912363fafb6bbfb527102ac06456d0d7c1b766c2
Edwyn <edwyns14@gmail.com>
2025-06-03 23:20:13 +0200

    Commit: "Hotfix: cambiar 15f por 16f"

--- src/sumadorproyecto/SumadorProyecto.java
+++ src/sumadorproyecto/SumadorProyecto.java
@@ -14,7 +14,8 @@ public class SumadorProyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(Sumador.sumar(15f, 4f));
+        System.out.println(Sumador.sumar(16f, 4f));
+
 
     }
     
